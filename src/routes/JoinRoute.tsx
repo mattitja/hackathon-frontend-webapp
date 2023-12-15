8e9535f18a7e7bd6bfbb0519d477b33161317dc5
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-15 23:47:34 +0100

    refactor: rm unnecessary code

--- src/routes/JoinRoute.tsx
+++ src/routes/JoinRoute.tsx
@@ -1,42 +1,41 @@
 import { useGame } from "../contexts/game";
 import { JoinActionDto } from "../generated/whackend/models/JoinActionDto";
+import styles from "./JoinRoute.module.css";
 
 export const JoinRoute = () => {
-  const { name, gameId, ws, setName, playerId, setGameId } = useGame();
+  const { ws, playerId } = useGame();
 
-  const handleJoinClick = () => {
+  const handleJoinClick = (e: SubmitEvent) => {
+    e.preventDefault();
+    var formEl = e.target as HTMLFormElement;
+    const form = new FormData(formEl);
+    const nickname = form.get('nickname') as string;
+    const gameId = form.get('gameId') as string || null;
+    console.log({ nickname, gameId });
+    
     const joinAction: JoinActionDto = {
       actionType: "JoinAction",
-      nickname: name(),
-      playerId: playerId(),
-      gameId: gameId().length == 4 ? gameId() : null,
+      nickname,
+      playerId,
+      gameId,
     };
     ws.send(JSON.stringify(joinAction));
   };
 
   return (
-    <section id="joinSection">
-      <div>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name()}
-            onInput={(e) => setName(e.target.value)}
-          />
+    <section class={styles.container}>
+      <form class={styles.form} onSubmit={handleJoinClick}>
+        <label class={styles.formField}>
+          <div>Name</div>
+          <input type="text" name="nickname" autofocus required />
         </label>
-        <br />
-        <label>
-          Game ID:
-          <input
-            type="text"
-            value={gameId()}
-            onInput={(e) => setGameId(e.target.value)}
-          />
+        <label class={styles.formField}>
+          <div>Game ID</div>
+          <small>Has a length of 4 characters</small>
+          <input type="text" name="gameId" pattern="\w{4}" />
         </label>
-        <br />
-        <button onClick={handleJoinClick}>Join</button>
-      </div>
+        <button type="submit">Join</button>
+      </form>
     </section>
   );
 };
