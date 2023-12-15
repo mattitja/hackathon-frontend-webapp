1c3506f7bf4db2138f48a39b29a67fdadec5e4d1
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-15 23:47:09 +0100

    chore: add markup for HomeRoute

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -1,12 +1,19 @@
 import { Component, JSXElement } from "solid-js";
 import { Console } from "../components/Console";
 import { GameProvider } from "../contexts/game";
+import styles from "./HomeRoute.module.css";
 
 export const HomeRoute: Component<{children?: JSXElement}> = (props) => {
   return (
     <GameProvider>
-      {props.children}
-      <Console />
+      <main class={styles.main}>
+        <div class={styles.outlet}>
+          {props.children}
+        </div>
+        <div class={styles.sidebar}>
+          <Console />
+        </div>
+      </main>
     </GameProvider>
   );
 };
