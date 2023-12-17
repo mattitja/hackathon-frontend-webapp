768d47071de41f9328c45e9f4b1d2cd51c3f8727
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-17 13:54:07 +0100

    chore: improve LobbyRoute

--- src/routes/LobbyRoute.tsx
+++ src/routes/LobbyRoute.tsx
@@ -1,36 +1,42 @@
-import { Show } from "solid-js";
+import { For, Show } from "solid-js";
 import { useGame } from "../contexts/game";
 import { GameCreateActionDto } from "../generated/whackend";
+import styles from "./LobbyRoute.module.css";
+import { useParams } from "@solidjs/router";
 
 export const LobbyRoute = () => {
-  const { gameId, playerId, ws, players } = useGame();
+  const { gameId } = useParams();
+  const { playerId, ws, players } = useGame();
 
   const handleCreateClick = () => {
     const action: GameCreateActionDto = {
       actionType: "GameCreateAction",
       playerId: playerId,
-      gameId: gameId(),
+      gameId,
     };
     ws.send(JSON.stringify(action));
   };
 
   return (
-    <section id="lobby">
-      <div>
-        <label>Game ID: {gameId()}</label>
-        <br />
-        <label>
-          Players
-          <ul>
-            {players().map(({ nickname }) => (
-              <li>{nickname}</li>
-            ))}
-          </ul>
-        </label>
-        <Show when={players().length >= 2}>
-          <button onClick={handleCreateClick}>Create Game</button>
-        </Show>
-      </div>
+    <section class={['stack', styles.container].join(' ')}>
+      <h1>Players</h1>
+      <ul class={['stack', styles.list].join(' ')}>
+        <For each={players()}>
+          {({ nickname }) => (
+            <li class={[
+                'fade-up-and-in',
+                'card',
+                styles.card
+              ].join(' ')}
+            >
+              {nickname}
+            </li>
+          )}
+        </For>
+      </ul>
+      <Show when={players().length >= 2}>
+        <button onClick={handleCreateClick}>Create Game</button>
+      </Show>
     </section>
   );
 };
