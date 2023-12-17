d856f54648eeebcd03601b916a0682ed6f658b7c
Lukas Koeller <acc.koeller@protonmail.com>
2023-12-17 13:52:33 +0100

    chore: add dynamic routing with :gameId

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,15 +5,18 @@ import { LobbyRoute } from "../../routes/LobbyRoute";
 import { PrepareRoute } from "../../routes/PrepareRoute";
 import { PlayRoute } from "../../routes/PlayRoute";
 import { HomeRoute } from "../../routes/HomeRoute";
+import { GameRoute } from "../../routes/GameRoute";
 
 const App: Component = () => {
   return (
     <Router>
       <Route path="/" component={HomeRoute}>
         <Route path="/join" component={JoinRoute} />
-        <Route path="/lobby" component={LobbyRoute} />
-        <Route path="/prepare" component={PrepareRoute} />
-        <Route path="/play" component={PlayRoute} />
+        <Route path="/:gameId" component={GameRoute}>
+          <Route path="/lobby" component={LobbyRoute} />
+          <Route path="/prepare" component={PrepareRoute} />
+          <Route path="/play" component={PlayRoute} />
+        </Route>
       </Route>
     </Router>
   );
